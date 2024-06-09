934e3b249dc85b7ce447fd0d01c2e393c622863c
Yaroslav Bilokrylets <4believy@gmail.com>
2024-06-09 13:28:25 +0300

    feat: setup json-server fake API, setup useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,62 @@
+import { useEffect, useReducer } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+
+type initialStateType = {
+  questions: [];
+  status: 'loading' | 'error' | 'ready' | 'active' | 'finish';
+};
+
+type actionType = {
+  type?: string;
+  payload?: [];
+};
+
+const initialState: initialStateType = { questions: [], status: 'loading' };
+
+function reducer(
+  state: initialStateType,
+  action: actionType,
+): initialStateType {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+
+    default:
+      throw new Error('Action unknown');
+  }
+}
 
 function App() {
-  return <>start</>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch(() => dispatch({ type: 'dataFailed' }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>question</p>
+      </Main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Main.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react';
+
+type Props = { children?: ReactNode };
+
+const Main = ({ children }: Props) => {
+  return <main className="main">{children}</main>;
+};
+export default Main;
